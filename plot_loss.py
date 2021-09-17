f66a326b8f86183a3abe3689082e09e5a56b1d34
Hiimmuc <nam1811.hust@gmail.com>
2021-09-17 09:04:51 +0700

    fix plot

--- plot_loss.py
+++ plot_loss.py
@@ -26,17 +26,29 @@ def plot_from_file(model, show=False):
 
     line_data = [line.strip().replace('\n', '').split(',')
                  for line in line_data]
-    data = []
+    data = [{}]
+    last_epoch = 1
+    step = 10
     for line in line_data:
         if 'IT' in line[0]:
-            data.append(line)
-    data_loss = [float(line[3].strip().split(' ')[1]) for line in data]
-    plot_graph(data_loss, 'epoch', 'loss', 'Loss',
-               f"exp/{model_name}/result/loss.png", color='b', mono=True, show=show)
-    data_acc = [float(line[2].strip().split(' ')[1]) for line in data]
-    plot_graph(data_acc, 'epoch', 'accuracy', 'Accuracy',
-               f"exp/{model_name}/result/acc.png", color='r', show=show)
-    plt.close()
+            epoch = int(line[0].split(' ')[-1])
+
+            if epoch not in range(last_epoch - step, last_epoch + 2):
+                data.append({})
+
+            data[-1][epoch] = line
+            last_epoch = epoch
+    # print(data)
+    for i, dt in enumerate(data):
+        data_loss = [float(line[3].strip().split(' ')[1])
+                     for _, line in dt.items()]
+        plot_graph(data_loss, 'epoch', 'loss', 'Loss',
+                   f"exp/{model_name}/result/loss_{i}.png", color='b', mono=True, show=show)
+        data_acc = [float(line[2].strip().split(' ')[1])
+                    for _, line in dt.items()]
+        plot_graph(data_acc, 'epoch', 'accuracy', 'Accuracy',
+                   f"exp/{model_name}/result/acc_{i}.png", color='r', show=show)
+        plt.close()
 
 
 parser = argparse.ArgumentParser(description="plot model")
@@ -45,4 +57,4 @@ if __name__ == '__main__':
                         default='ResNetSE34V2', help='model name')
     args = parser.parse_args()
     model_name = args.model
-    plot_from_file(model_name, show=True)
+    plot_from_file(model_name, show=False)
