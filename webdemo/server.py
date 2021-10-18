9a1ee03d3fe7869d84c75d912fe8e349154df8ab
Hiimmuc <you@example.comnam1811.hust@gmail.com>
2021-10-18 10:10:48 +0700

    update html

--- webdemo/server.py
+++ webdemo/server.py
@@ -6,6 +6,7 @@ from json import dumps
 from flask import (Flask, Markup, flash, jsonify, redirect, render_template,
                    request, send_file, send_from_directory, url_for)
 from flask_restful import Api, Resource
+# from model import SpeakerNet
 from sqlalchemy import create_engine
 from werkzeug.utils import secure_filename
 
@@ -13,11 +14,13 @@ from sqlconnect import *
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'super secret key'
-UPLOAD_FOLDER = "static/uploads"
+UPLOAD_FOLDER = "webdemo/static/uploads"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-ALLOWED_EXTENSIONS = set(['.wav', '.mp4'])
+ALLOWED_EXTENSIONS = set(['wav', 'mp4'])
 api = Api(app)
+# load model
+# model = SpeakerNet()
 
 
 def allowed_file(filename):
@@ -31,37 +34,35 @@ def home():
 
 
 @app.route('/audio/<filename>')
-def images(filename):
-    image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    return send_file(image_path)
+def audio(filename):
+    audio_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_file(audio_path)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_audio():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'files[]' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        if file in allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(full_path)
+        files = request.files.getlist('files[]')
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                return redirect('/')
+        # predict
+        # make prediction here
+        # return and upload score
+        t0 = time.time()
+        result = None
+        inference_time = time.time() - t0
 
-            # predict
-            # make prediction here
-            # return and upload score
-            t0 = time.time()
-            result = None
-            inference_time = time.time() - t0
-
-            return render_template('demo.html',
-                                   result=result,
-                                   inference_time=inference_time)
-        else:
-            return redirect("/")
+        return render_template('demo.html',
+                               result=result,
+                               inference_time=inference_time)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
