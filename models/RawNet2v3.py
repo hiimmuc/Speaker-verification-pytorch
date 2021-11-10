28aceeb7ccc9bf951e5909b40873ab28d26c4a55
Bernard Deng <56968739+hiimmuc@users.noreply.github.com>
2021-11-10 09:29:20 +0700

    Update RawNet2v3.py

--- models/RawNet2v3.py
+++ models/RawNet2v3.py
@@ -11,8 +11,10 @@ from numpy.core.fromnumeric import transpose
 from torch.autograd import Variable
 from torch.nn.parameter import Parameter
 from torch.utils import data
-from torchsummary import summary
-# from utils import *
+
+import torch.backends.cudnn
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
 
 
 class RawNetBasicBlock(nn.Module):
@@ -89,7 +91,159 @@ class AFMS(nn.Module):
         x = x + self.alpha
         x = x * y
         return x
+    
+    
+class LayerNorm(nn.Module):
 
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm,self).__init__()
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        device = x.get_device()
+        
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        
+        gamma = self.gamma.to(device)
+        beta = self.beta.to(device)
+
+        return gamma * (x - mean) / (std + self.eps) + beta
+
+class SincConv_fast(nn.Module):
+    """Sinc-based convolution
+    Parameters
+    ----------
+    in_channels : `int`
+        Number of input channels. Must be 1.
+    out_channels : `int`
+        Number of filters.
+    kernel_size : `int`
+        Filter length.
+    sample_rate : `int`, optional
+        Sample rate. Defaults to 16000.
+    Usage
+    -----
+    See `torch.nn.Conv1d`
+    Reference
+    ---------
+    Mirco Ravanelli, Yoshua Bengio,
+    "Speaker Recognition from raw waveform with SincNet".
+    https://arxiv.org/abs/1808.00158
+    """
+
+    @staticmethod
+    def to_mel(hz):
+        return 2595 * np.log10(1 + hz / 700)
+
+    @staticmethod
+    def to_hz(mel):
+        return 700 * (10 ** (mel / 2595) - 1)
+
+    def __init__(self, out_channels, kernel_size, sample_rate=16000, in_channels=1,
+                 stride=1, padding=0, dilation=1, bias=False, groups=1, min_low_hz=50, min_band_hz=50):
+
+        super(SincConv_fast,self).__init__()
+
+        if in_channels != 1:
+            #msg = (f'SincConv only support one input channel '
+            #       f'(here, in_channels = {in_channels:d}).')
+            msg = "SincConv only support one input channel (here, in_channels = {%i})" % (in_channels)
+            raise ValueError(msg)
+
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        
+        # Forcing the filters to be odd (i.e, perfectly symmetrics)
+        if kernel_size%2==0:
+            self.kernel_size=self.kernel_size+1
+            
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+
+        if bias:
+            raise ValueError('SincConv does not support bias.')
+        if groups > 1:
+            raise ValueError('SincConv does not support groups.')
+
+        self.sample_rate = sample_rate
+        self.min_low_hz = min_low_hz
+        self.min_band_hz = min_band_hz
+
+        # initialize filterbanks such that they are equally spaced in Mel scale
+        low_hz = 30
+        high_hz = self.sample_rate / 2 - (self.min_low_hz + self.min_band_hz)
+
+        mel = np.linspace(self.to_mel(low_hz),
+                          self.to_mel(high_hz),
+                          self.out_channels + 1)
+        hz = self.to_hz(mel)
+        
+
+        # filter lower frequency (out_channels, 1)
+        self.low_hz_ = nn.Parameter(torch.Tensor(hz[:-1]).view(-1, 1))
+
+        # filter frequency band (out_channels, 1)
+        self.band_hz_ = nn.Parameter(torch.Tensor(np.diff(hz)).view(-1, 1))
+
+        # Hamming window
+        #self.window_ = torch.hamming_window(self.kernel_size)
+        n_lin=torch.linspace(0, (self.kernel_size/2)-1, steps=int((self.kernel_size/2))) # computing only half of the window
+        self.window_=0.54-0.46*torch.cos(2*math.pi*n_lin/self.kernel_size);
+
+        # (1, kernel_size/2)
+        n = (self.kernel_size - 1) / 2.0
+        self.n_ = 2*math.pi*torch.arange(-n, 0).view(1, -1) / self.sample_rate # Due to symmetry, I only need half of the time axes
+
+    def forward(self, waveforms):
+        """
+        Parameters
+        ----------
+        waveforms : `torch.Tensor` (batch_size, 1, n_samples)
+            Batch of waveforms.
+        Returns
+        -------
+        features : `torch.Tensor` (batch_size, out_channels, n_samples_out)
+            Batch of sinc filters activations.
+        """
+
+        self.n_ = self.n_.to(waveforms.device)
+
+        self.window_ = self.window_.to(waveforms.device)
+        
+        low_hz_ = self.low_hz_.to(waveforms.device)
+        band_hz_ = self.band_hz_.to(waveforms.device)
+        
+        low = self.min_low_hz  + torch.abs(low_hz_)
+        
+        
+        
+        high = torch.clamp(low + self.min_band_hz + torch.abs(band_hz_), self.min_low_hz, self.sample_rate/2)
+        band=(high-low)[:,0]
+        
+        f_times_t_low = torch.matmul(low, self.n_)
+        f_times_t_high = torch.matmul(high, self.n_)
+
+        band_pass_left=((torch.sin(f_times_t_high)-torch.sin(f_times_t_low))/(self.n_/2))*self.window_ # Equivalent of Eq.4 of the reference paper (SPEAKER RECOGNITION FROM RAW WAVEFORM WITH SINCNET). I just have expanded the sinc and simplified the terms. This way I avoid several useless computations. 
+        band_pass_center = 2*band.view(-1,1)
+        band_pass_right= torch.flip(band_pass_left,dims=[1])
+        
+        
+        band_pass=torch.cat([band_pass_left,band_pass_center,band_pass_right],dim=1)
+
+        
+        band_pass = band_pass / (2*band[:,None])
+        
+
+        self.filters_map = (band_pass).view(
+            self.out_channels, 1, self.kernel_size)
+
+        return F.conv1d(waveforms, self.filters_map, stride=self.stride,
+                        padding=self.padding, dilation=self.dilation,
+                         bias=None, groups=1)
 
 class RawNet2(nn.Module):
     """
@@ -108,8 +262,12 @@ class RawNet2(nn.Module):
             n_mels=64,
             log_input=True,
             nb_gru_layers =1, gru_node=1024,
+            nb_fc_node=1024, 
+            first_conv_size=251,
+            nb_samp=16240,
             **kwargs):
         super(RawNet2, self).__init__()
+#         self.device = kwargs['device']
         self.inplanes = nb_filters[0]
         self.n_mels = n_mels
         self.log_input = log_input
@@ -123,6 +281,14 @@ class RawNet2(nn.Module):
             stride=3,
             padding=0
         )
+        #  sinc layer
+        self.ln = LayerNorm(nb_samp)
+        self.first_conv = SincConv_fast(in_channels = in_channels,
+            out_channels = nb_filters[0],
+            kernel_size = first_conv_size
+            )
+
+        self.first_bn = nn.BatchNorm1d(nb_filters[0])
 
         #####
         # residual blocks for frame-level representations
@@ -137,7 +303,7 @@ class RawNet2(nn.Module):
         #####
         # aggregate to utterance(segment)-level
         #####
-        self.bn_before_agg = nn.BatchNorm1d(gru_node)
+        self.bn_before_agg = nn.BatchNorm1d(nb_filters[5])
         self.attention = nn.Sequential(
             nn.Conv1d(nb_filters[5], 128, kernel_size=1),
             nn.LeakyReLU(),
@@ -150,7 +316,9 @@ class RawNet2(nn.Module):
         # speaker embedding layer
         #####
         self.fc = nn.Linear(nb_filters[5] * 2, code_dim)  # speaker embedding layer
-        self.lrelu = nn.LeakyReLU(0.3)  # keras style
+        
+        self.lrelu = nn.LeakyReLU()
+        self.lrelu_keras = nn.LeakyReLU(negative_slope = 0.3)
 
         #####
         # initialize
@@ -191,11 +359,21 @@ class RawNet2(nn.Module):
 
     def forward(self, x):
         #####
+        device = x.get_device()
+        x = self.ln(x)
         x = x.unsqueeze(1)
         #####
         # first layers before residual blocks
         #####
-        x = self.conv1(x)
+#         x = self.conv1(x)
+#         
+#         nb_samp = x.shape[0]
+#         len_seq = x.shape[1]
+#         x = self.ln(x)
+#         x=x.view(nb_samp,1,len_seq)
+        x = F.max_pool1d(torch.abs(self.first_conv(x)), 3).to(device)
+        x = self.first_bn(x)
+        x = self.lrelu_keras(x)
 
         #####
         # frame-level
@@ -206,34 +384,33 @@ class RawNet2(nn.Module):
         x = self.layer4(x)
         x = self.layer5(x)
         x = self.layer6(x)
-
-
+        
         #####
         # gru
         #####
-        x = self.bn_before_gru(x)
-        x = self.lrelu(x)
-        x = x.permute(0, 2, 1)  #(batch, filt, time) >> (batch, time, filt)
-        self.gru.flatten_parameters()
-        x, _ = self.gru(x)
-        x = x[:,-1,:]
-        x = self.fc_after_gru(x)
+#         x = self.bn_before_gru(x)
+#         x = self.lrelu_keras(x)
+#         x = x.permute(0, 2, 1)  #(batch, filt, time) >> (batch, time, filt)
+#         self.gru.flatten_parameters()
+#         x, _ = self.gru(x)
+#         x = x[:,-1,:]
+#         x = self.fc_after_gru(x)
         
-#         #####
-#         # aggregation: attentive statistical pooling
-#         #####
-#         x = self.bn_before_agg(x)
-#         x = self.lrelu(x)
-#         w = self.attention(x)
-#         m = torch.sum(x * w, dim=-1)
-#         s = torch.sqrt((torch.sum((x ** 2) * w, dim=-1) - m ** 2).clamp(min=1e-5))
-#         x = torch.cat([m, s], dim=1)
-#         x = x.view(x.size(0), -1)
+        #####
+        # aggregation: attentive statistical pooling
+        #####
+        x = self.bn_before_agg(x)
+        x = self.lrelu_keras(x)
+        w = self.attention(x)
+        m = torch.sum(x * w, dim=-1)
+        s = torch.sqrt((torch.sum((x ** 2) * w, dim=-1) - m ** 2).clamp(min=1e-5))
+        x = torch.cat([m, s], dim=1)
+        x = x.view(x.size(0), -1)
 
-#         #####
-#         # speaker embedding layer
-#         #####
-#         x = self.fc(x)
+        #####
+        # speaker embedding layer
+        #####
+        x = self.fc(x)
 
         return x
 
