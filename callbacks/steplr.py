458f5153a19245aef21d6b31033057ec60a4bdf8
Bernard Deng <56968739+hiimmuc@users.noreply.github.com>
2022-01-27 15:31:14 +0700

    Add files via upload

--- callbacks/steplr.py
+++ callbacks/steplr.py
@@ -3,9 +3,9 @@ import torch
 # taken from https://debuggercafe.com/using-learning-rate-scheduler-and-early-stopping-with-pytorch/
 
 
-def Scheduler(optimizer, test_interval, lr_decay, **kwargs):
+def Scheduler(optimizer, step_size, lr_decay, **kwargs):
     sche_fn = torch.optim.lr_scheduler.StepLR(optimizer,
-                                              step_size=test_interval,
+                                              step_size=step_size,
                                               gamma=lr_decay)
     lr_step = 'epoch'
     # print('Initialised step LR scheduler')
