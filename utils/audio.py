6224fc91e4b7330f55e07a87d153e942f0b40b84
Bernard Deng <56968739+hiimmuc@users.noreply.github.com>
2022-02-10 15:26:10 +0700

    Update audio.py

--- utils/audio.py
+++ utils/audio.py
@@ -10,44 +10,6 @@ import soundfile as sf
 from tqdm import tqdm
 from pathlib import Path
 import os
-from pydub import AudioSegment
-
-
-def convert_audio(audio_path, new_format='wav', freq=8000, out_path=None):
-    """Convert audio format and samplerate to target"""
-    try:
-        org_format = audio_path.split('.')[-1].strip()
-        if new_format != org_format:
-            audio = AudioSegment.from_file(audio_path)
-            # export file as new format
-            audio_path = audio_path.replace(org_format, new_format)
-            audio.export(audio_path, format=new_format)
-    except Exception as e:
-        raise e
-        
-    try:
-        sound = AudioSegment.from_file(audio_path, format='wav')
-        sound = sound.set_frame_rate(freq)
-        sound = sound.set_channels(1)
-        
-        if out_path is not None:
-            out_path = audio_path
-        sound.export(audio_path, format='wav')
-    except Exception as e:
-        raise e
-        
-    return audio_path
-
-def change_volume(path=None, dB=6, overwrite=True):
-    segment = AudioSegment.from_file(path)
-    segment += dB
-    if overwrite:
-        path = path
-    else:
-        path = path.replace('.wav', '') + f'_{dB}' +'.wav'
-    segment.export(path, format='wav')
-    print(f'Export to {path}')
-
 
 def get_duration_file(fn_audio):
     """Getting duration information of audio files
@@ -92,75 +54,6 @@ def get_size_file(fname):
 def get_size_folder(folder):
     return sum([float(get_size_file(f)) for f in glob.glob(f"{folder}/*")])
 
-
-def get_audio_ffmpeg_astats(filename):
-    """Get stats information of audio 
-    Display time domain statistical information about the audio channels. Statistics are calculated and displayed for each audio channel and, where applicable, an overall figure is also given.
-
-    It accepts the following option:
-
-    length: Short window length in seconds, used for peak and trough RMS measurement. Default is 0.05 (50 milliseconds). Allowed range is [0 - 10].
-
-    metadata: Set metadata injection. All the metadata keys are prefixed with lavfi.astats.X, where X is channel number starting from 1 or string Overall. Default is disabled.
-    Available keys for each channel are: DC_offset Min_level Max_level Min_difference Max_difference Mean_difference RMS_difference Peak_level RMS_peak RMS_trough Crest_factor Flat_factor Peak_count Noise_floor Noise_floor_count Entropy Bit_depth Dynamic_range Zero_crossings Zero_crossings_rate Number_of_NaNs Number_of_Infs Number_of_denormals
-    and for Overall: DC_offset Min_level Max_level Min_difference Max_difference Mean_difference RMS_difference Peak_level RMS_level RMS_peak RMS_trough Flat_factor Peak_count Noise_floor Noise_floor_count Entropy Bit_depth Number_of_samples Number_of_NaNs Number_of_Infs Number_of_denormals
-    For example full key look like this lavfi.astats.1.DC_offset or this lavfi.astats.Overall.Peak_count.
-
-    For description what each key means read below.
-    - reset: Set the number of frames over which cumulative stats are calculated before being reset Default is disabled.
-    - measure_perchannel: Select the parameters which are measured per channel. The metadata keys can be used as flags, default is all which measures everything. none disables all per channel measurement.
-    - measure_overall: Select the parameters which are measured overall. The metadata keys can be used as flags, default is all which measures everything. none disables all overall measurement.
-    A description of each shown parameter follows:
-
-    DC offset: Mean amplitude displacement from zero.
-
-    Min level: Minimal sample level.
-
-    Max level: Maximal sample level.
-
-    Min difference: Minimal difference between two consecutive samples.
-
-    Max difference: Maximal difference between two consecutive samples.
-
-    Mean difference: Mean difference between two consecutive samples. The average of each difference between two consecutive samples.
-
-    RMS difference: Root Mean Square difference between two consecutive samples.
-
-    Peak level dB & RMS level dB: Standard peak and RMS level measured in dBFS.
-
-    RMS peak dB & RMS trough dB: Peak and trough values for RMS level measured over a short window.
-
-    Crest factor: Standard ratio of peak to RMS level (note: not in dB).
-
-    Flat factor: Flatness (i.e. consecutive samples with the same value) of the signal at its peak levels (i.e. either Min level or Max level).
-
-    Peak count: Number of occasions (not the number of samples) that the signal attained either Min level or Max level.
-
-    Noise floor dB: Minimum local peak measured in dBFS over a short window.
-
-    Noise floor count: Number of occasions (not the number of samples) that the signal attained Noise floor.
-
-    Entropy: Entropy measured across whole audio. Entropy of value near 1.0 is typically measured for white noise.
-
-    Bit depth: Overall bit depth of audio. Number of bits used for each sample.
-
-    Dynamic range: Measured dynamic range of audio in dB.
-
-    Zero crossings: Number of points where the waveform crosses the zero level axis.
-
-    Zero crossings rate: Rate of Zero crossings and number of audio samples
-
-    Args:
-        filename ([str]): path to audio file
-
-    Returns:
-        dict: dictionary of information
-    """
-    cmd = ['ffmpeg', '-i', filename, '-map', '0:a', '-af', 'astats', '-f', 'null', '-']
-    out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stderr
-    output_lines = [line.strip() for line in out.decode('utf-8').split('\n')]
-    return output_lines
-
 def get_dataset_general_infor(root="dataset/train"):
     root = Path(root)
     # get numbr of file
@@ -184,3 +77,98 @@ def get_dataset_general_infor(root="dataset/train"):
 #         audio_folder_amplitute[audio_folder.name] = list([get_amplitute_file(audio_file)[-1] for audio_file in audio_folder.iterdir()])
     
     return audio_folder_num, audio_folder_duration, audio_folder_size
+
+
+def get_audio_ffmpeg_astats(filename):
+    """Get stats information of audio 
+    Display time domain statistical information about the audio channels. Statistics are calculated and displayed for each audio channel and, where applicable, an overall figure is also given.
+
+    It accepts the following option:
+
+    length: Short window length in seconds, used for peak and trough RMS measurement. Default is 0.05 (50 milliseconds). Allowed range is [0 - 10].
+
+    metadata: Set metadata injection. All the metadata keys are prefixed with lavfi.astats.X, where X is channel number starting from 1 or string Overall. Default is disabled.
+    Available keys for each channel are: DC_offset Min_level Max_level Min_difference Max_difference Mean_difference RMS_difference Peak_level RMS_peak RMS_trough Crest_factor Flat_factor Peak_count Noise_floor Noise_floor_count Entropy Bit_depth Dynamic_range Zero_crossings Zero_crossings_rate Number_of_NaNs Number_of_Infs Number_of_denormals
+    and for Overall: DC_offset Min_level Max_level Min_difference Max_difference Mean_difference RMS_difference Peak_level RMS_level RMS_peak RMS_trough Flat_factor Peak_count Noise_floor Noise_floor_count Entropy Bit_depth Number_of_samples Number_of_NaNs Number_of_Infs Number_of_denormals
+    For example full key look like this lavfi.astats.1.DC_offset or this lavfi.astats.Overall.Peak_count.
+
+    For description what each key means read below.
+    - reset: Set the number of frames over which cumulative stats are calculated before being reset Default is disabled.
+    - measure_perchannel: Select the parameters which are measured per channel. The metadata keys can be used as flags, default is all which measures everything. none disables all per channel measurement.
+    - measure_overall: Select the parameters which are measured overall. The metadata keys can be used as flags, default is all which measures everything. none disables all overall measurement.
+    A description of each shown parameter follows:
+
+    - DC offset: Mean amplitude displacement from zero.
+
+    - Min level: Minimal sample level.
+
+    - Max level: Maximal sample level.
+
+    - Min difference: Minimal difference between two consecutive samples.
+
+    - Max difference: Maximal difference between two consecutive samples.
+
+    - Mean difference: Mean difference between two consecutive samples. The average of each difference between two consecutive samples.
+
+    - RMS difference: Root Mean Square difference between two consecutive samples.
+
+    - Peak level dB & RMS level dB: Standard peak and RMS level measured in dBFS.
+
+    - RMS peak dB & RMS trough dB: Peak and trough values for RMS level measured over a short window.
+
+    - Crest factor: Standard ratio of peak to RMS level (note: not in dB).
+
+    - Flat factor: Flatness (i.e. consecutive samples with the same value) of the signal at its peak levels (i.e. either Min level or Max level).
+
+    - Peak count: Number of occasions (not the number of samples) that the signal attained either Min level or Max level.
+
+    - Noise floor dB: Minimum local peak measured in dBFS over a short window.
+
+    - Noise floor count: Number of occasions (not the number of samples) that the signal attained Noise floor.
+
+    - Entropy: Entropy measured across whole audio. Entropy of value near 1.0 is typically measured for white noise.
+
+    - Bit depth: Overall bit depth of audio. Number of bits used for each sample.
+
+    - Dynamic range: Measured dynamic range of audio in dB.
+
+    - Zero crossings: Number of points where the waveform crosses the zero level axis.
+
+    - Zero crossings rate: Rate of Zero crossings and number of audio samples
+
+    Args:
+        filename ([str]): path to audio file
+
+    Returns:
+        dict: dictionary of information
+    """
+    cmd = ['ffmpeg', '-i', filename, '-map', '0:a', '-af', 'astats', '-f', 'null', '-']
+    out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stderr
+    output_lines = [line.strip() for line in out.decode('utf-8').split('\n')]
+    
+    
+    # filter
+    header = ['Duration', 'Size', 'Min level', 'Max level', 
+              'Min difference', 'Max difference', 'Mean difference', 'RMS difference', 
+              'Peak level dB', 'RMS level dB',   'RMS peak dB', 'RMS trough dB', 
+              'Crest factor', 'Flat factor', 'Peak count',
+              'Noise floor dB', 'Noise floor count', 'Bit depth', 'Dynamic range', 
+              'Zero crossings', 'Zero crossings rate']
+    details = {}
+    condition = lambda x: 'Parsed_astats_0' in x
+    filtered_lines = list(filter(condition, output_lines))
+
+    for line in filtered_lines:
+        detail = line.replace(f"[{line.split('[')[-1].split(']')[0]}]", '').strip().split(':')
+        if detail[0] == 'Overall':
+            continue
+        details[detail[0]] = detail[1]
+    for k in header:
+        if k not in details:
+            details[k] = None
+    
+    
+    details['Duration'] = get_duration_file(filename)
+    details['Size'] = get_size_file(filename)
+    
+    return details
